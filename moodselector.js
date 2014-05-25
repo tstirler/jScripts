2c7fb9100efa47964d90eddcba1c613ec9472445
tstirler <torbjorn@stirler.no>
2014-05-25 14:00:05 +0200

    moods

--- moodselector.js
+++ moodselector.js
@@ -1,4 +1,11 @@
 var onCall = true;
 var weekday = ["moday", "tuesday", "wednsday", "thursday", "friday", "saturday", "sunday"];
-var mood = ["good", "fine", "ok", "meh", "grumpy", "bah"];
+var mood = ["awesome", "good", "fine", "ok", "meh", "grumpy", "bah"];
+var moodOffset = 0
 
+if onCall {
+  moodOffset = 1
+
+} else {
+  moodOffset = 0
+}
